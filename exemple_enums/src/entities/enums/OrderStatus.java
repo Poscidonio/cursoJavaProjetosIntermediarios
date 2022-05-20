c5ac8cb6f2d282a0b8a8f9dff01fde5f95f66514
Poscidonio <guilhermeposcidonio@gmail.com>
2022-05-20 17:02:13 -0300

    Organizando novamente

---
+++ exemple_enums/src/entities/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package entities.enums;
+
+public enum OrderStatus {
+	
+	//constantes declaradas literalmente que serao utilizadas para saber o status do pedido
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERD;
+
+}
